2ac4116b6757aec6b14c56368cbc3234621129fc
Fern <fernandos.magee@gmail.com>
2024-02-24 03:48:39 -0500

    11 rooms.

---
+++ scripts/room.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+@export var entrance_height: int = 4
+@export var exit_height: int = 4
+@export var room_width: int = 4
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
